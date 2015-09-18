f8759d6af4d1c2de9407de7d00a61fd6c10c2790
unknown <lukashid@gmail.com>
2015-09-18 23:00:40 +0200

    add SherlockSquares

---
+++ Implementation/SherlockandSquares/SherlockandSquares/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace SherlockandSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint T = Convert.ToUInt16(Console.ReadLine());
+            for (int i = 0 ; i < T; ++i)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                ulong leftRange = Convert.ToUInt64(Math.Ceiling(Math.Sqrt(Convert.ToUInt64(input[0]))));
+                ulong rightRange = Convert.ToUInt64(Math.Floor(Math.Sqrt(Convert.ToUInt64(input[1]))));
+
+                ulong counter = rightRange - leftRange + 1;
+
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
