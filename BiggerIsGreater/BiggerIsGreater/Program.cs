0fe456d28364a576653af6b935abea063702376a
lukashid <lukashid@gmail.com>
2016-02-21 20:45:25 +0100

    new challenges

---
+++ BiggerIsGreater/BiggerIsGreater/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace BiggerIsGreater
+{
+    static class Program
+    {
+        public static string SwapInString(string str, int pos1, int pos2)
+        {
+            StringBuilder stringBuilder = new StringBuilder(str);
+            char temp = stringBuilder[pos1];
+            stringBuilder[pos1] = stringBuilder[pos2];
+            stringBuilder[pos2] = temp;
+            return stringBuilder.ToString();
+        }
+
+        public static int EvalPower(string word)
+        {
+            int sum = 0;
+            for(int i = 0; i < word.Length; i++)
+            {
+                sum += (word.Length - i) * word[i];
+            }
+            return sum;
+        }
+
+        public static bool IsGreaterThan(this string word1, string word2)
+        {
+            int powerWord1 = EvalPower(word1);
+            int powerWord2 = EvalPower(word2);
+
+            if (powerWord1 > powerWord2) return true;
+            return false;
+        }
+
+        public static void ReturnGreater(string word)
+        {
+            string sortWord = String.Concat(word.OrderBy(c => c));
+            bool foundGreater = false;
+            string greaterWord = null;
+            
+
+
+            if(!foundGreater) Console.WriteLine("no answer");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvalPower("hefg"));
+            int t = Convert.ToInt32(Console.ReadLine());
+            string[] arr = new string[t];
+
+            for(int i = 0; i < t; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i < t; i++)
+            {
+                ReturnGreater(arr[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Implementation/CavityMap/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+class Solution
+{
+
+    static bool IsCavityField(int depth, int[] neighbors)
+    {
+        if (neighbors[0] < depth && neighbors[1] < depth &&
+            neighbors[2] < depth && neighbors[3] < depth)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] grid = new string[n];
+        StringBuilder[] gridResult = new StringBuilder[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            grid[i] = Console.ReadLine();
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            StringBuilder iString = new StringBuilder(n);
+            for(int j = 0; j < n; j++)
+            {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) iString.Append(grid[i][j]);
+                else if (IsCavityField(grid[i][j],
+                    new int[] {
+                        grid[i][j-1],
+                        grid[i-1][j],
+                        grid[i][j+1],
+                        grid[i+1][j]
+                })) iString.Append("X");  else iString.Append(grid[i][j]);
+            }
+            gridResult[i] = iString;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(gridResult[i]);
+        }
+    }
+}
